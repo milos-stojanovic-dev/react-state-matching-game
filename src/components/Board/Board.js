05860f098568806b8a58526e44f6df958ae559a0
Milos Stojanovic <Milos.Stojanovic@flagstoneim.com>
2021-02-24 15:40:41 +0100

    Commit #1 MS

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,18 +1,20 @@
-import React from 'react'
-import './Board.css';
+import React from "react";
+import "./Board.css";
+import Tile from "../Tile";
 
 const Board = (props) => {
-
   const gridConfig = {
     gridTemplateColumns: `repeat(${Math.sqrt(props.numTiles)}, 1fr)`,
     gridTemplateRows: `repeat(${Math.sqrt(props.numTiles)}, 1fr)`,
-  }
+  };
 
-
+  const mapTiles = props.tiles.map((tile) => <Tile {...tile} />);
+  console.log(mapTiles);
   return (
-    <div className='Board' style={gridConfig}>
+    <div className="Board" style={gridConfig}>
+      {mapTiles}
     </div>
-  )
-}
+  );
+};
 
-export default Board
+export default Board;
--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from "react";
 
-import './Tile.css'
+import "./Tile.css";
 
-const Tile = () => {
+const Tile = (props) => {
+  const color =
+    props.selected || props.matched ? { backgroundColor: props.color } : null;
 
   return (
-    <div className='Tile'>
+    <div className="Tile">
+      {(props.selected || props.matched) && <svg>{props.svg}</svg>}
     </div>
-  )
-}
+  );
+};
 
-export default Tile
+export default Tile;
