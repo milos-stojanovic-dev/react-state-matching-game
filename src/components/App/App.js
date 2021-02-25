043921521f310985746c38a8372c2cc76730783c
Milos Stojanovic <Milos.Stojanovic@flagstoneim.com>
2021-02-25 11:40:12 +0100

    commit MS new

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import OptionsPanel from "../OptionsPanel";
 import Board from "../Board";
-import { createTiles } from "../../misc/utils";
+import { createTiles, indexOfSelected } from "../../misc/utils";
 
 import "./App.css";
 
@@ -17,16 +17,55 @@ class App extends Component {
     };
   }
 
+  handleTileClicked = (id, color) => {
+    this.setState(() => {
+      const tiles = this.state.tiles;
+      let toBeCleared = this.state.toBeCleared;
+      const selectedTileIndex = indexOfSelected(tiles, id, color);
+      let previousTileIndex = this.state.previousTileIndex;
+
+      if (toBeCleared !== null) {
+        let tile0 = toBeCleared[0];
+        let tile1 = toBeCleared[1];
+        tiles[tile0].selected = false;
+        tiles[tile1].selected = false;
+        toBeCleared = null;
+      }
+
+      tiles[selectedTileIndex].selected = true;
+
+      if (previousTileIndex !== null) {
+        let previousTile = tiles[previousTileIndex];
+        let selectedTile = tiles[selectedTileIndex];
+        if (previousTile.id !== selectedTile.id && previousTile.color === color) {
+          selectedTile.matched = true;
+          previousTile.matched = true;
+          previousTileIndex = null;
+        } else {
+          toBeCleared = [previousTileIndex, selectedTileIndex];
+          previousTileIndex = null;
+        }
+      } else {
+        previousTileIndex = selectedTileIndex;
+      }
+      return {
+        tiles,
+        toBeCleared,
+        previousTileIndex
+      };
+    });
+  };
+
   startGame = (numTiles) => {
     this.setState((state) => {
       return {
         playing: true,
         previousTileIndex: null,
         toBeCleared: null,
-        tiles: createTiles(this.state.numTiles),
+        tiles: createTiles(this.state.numTiles, this.handleTileClicked),
       };
     });
-  }
+  };
 
   render() {
     return (
